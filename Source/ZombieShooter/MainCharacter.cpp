5cb1d459f8b2dc2e9bfc6527838bd03e6fdd3bd3
Justin Rivera <jrivera26333@gmail.com>
2022-08-28 16:31:56 -0500

    Game Mode and Character Created

---
+++ Source/ZombieShooter/MainCharacter.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainCharacter.h"
+#include "Components/InputComponent.h"
+
+// Sets default values
+AMainCharacter::AMainCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMainCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMainCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMainCharacter::ForwardMovement);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMainCharacter::RightMovement);
+}
+
+void AMainCharacter::ForwardMovement(float AxisValue)
+{
+	AddMovementInput(GetActorForwardVector() * AxisValue);
+}
+
+void AMainCharacter::RightMovement(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector() * AxisValue);
+}
+
---
+++ Source/ZombieShooter/ZombieGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ZombieGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIESHOOTER_API AZombieGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
